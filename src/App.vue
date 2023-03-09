7aeb257ebea0abdfda5ea27d582dc976f5c2fda9
Alex <alex.quemeneur17@gmail.com>
2023-03-09 11:58:11 +0100

    utils & load components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,52 @@
-<script setup>
-</script>
+
 
 <template>
-  <h1>Hello</h1>
-  <router-view />
+  <div :class="this.theme === 'light' ? 'light-theme' : 'dark-theme'">
+    <div class="theme">
+      <button @click.prevent="changeTheme()" >Changer de thème</button>
+    </div>
+    <HeaderComponent />
+    <div class="main-content">
+      <router-view />
+    </div>
+    <FooterComponent />
+  </div>
 </template>
+
+
+
+<script>
+import local from './utils/local.js';
+import HeaderComponent from './components/base/HeaderComponent.vue';
+import FooterComponent from './components/base/FooterComponent.vue';
+export default {
+  name: 'App',
+  data(){
+    return{       
+      theme: "",
+    }
+  },
+  components: {
+    HeaderComponent,
+    FooterComponent,
+  },
+  async created(){
+    let color = local.get('theme')
+    if(color){
+      this.theme = local.get('theme')
+    }else{
+      this.theme = 'light'
+    }
+  },
+  methods:{
+        changeTheme(){
+            this.theme = this.theme === "dark" ? "light" : "dark";
+            local.set('theme', this.theme); 
+        }
+    }
+}
+</script>
+
 
 <style lang="scss" scoped>
 
